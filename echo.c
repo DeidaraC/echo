0e0d1f51b6e5418293c279531bb569e504491cf4
JIANG Di <jiangdirb@gmail.com>
2015-10-01 22:03:55 +0800

    Fix corner case: `echo -n`

--- echo.c
+++ echo.c
@@ -7,17 +7,20 @@ int main(int argc, const char *argv[]) {
   // the `-n` option, see `man echo`
   bool nflag;
 
-  if ((argc > 1) && !strcmp(argv[1], "-n")) {
+  argv++;
+
+  if ((argc > 1) && !strcmp(*argv, "-n")) {
     nflag = true;
+    argv++;
   } else {
     nflag = false;
   }
 
-  if (argc > 1) {
-    printf("%s", argv[1]);
+  if (*argv) {
+    printf("%s", *argv);
 
     for (size_t i = 2; i < argc; i++) {
-      printf(" %s", argv[i]);
+      printf(" %s", *(argv += 1));
     }
   }
 
