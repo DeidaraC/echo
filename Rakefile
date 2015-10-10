a1f8fd4549d30daec0b2fdd2708f6594d7e04133
JIANG Di <jiang-di@outlook.com>
2015-10-11 00:35:05 +0800

    Add test

--- Rakefile
+++ Rakefile
@@ -1,4 +1,5 @@
-task :default => :format
+require "rake/testtask"
+task :default => :test
 
 desc "format code"
 task :format do
@@ -10,4 +11,8 @@ task :format do
       file.puts(output)
     end
   end
+end
+
+Rake::TestTask.new do |t|
+  t.test_files = FileList["test.rb"]
 end
---
+++ test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+
+class TestEcho < Minitest::Test
+  `cc echo.c -o echo.out`
+
+  def test_basic_usage
+    output = `./echo.out hello, world!`
+    assert_equal "hello, world!\n", output
+  end
+
+  def test_single_quotes
+    output = `./echo.out 'hello, world!'`
+    assert_equal "hello, world!\n", output
+  end
+
+  def test_double_quotes
+    output = `./echo.out "hello, world!"`
+    assert_equal "hello, world!\n", output
+  end
+
+  def test_flag_n
+    assert_equal "hello, world!", `./echo.out -n hello, world!`
+    assert_equal "hello, world!", `./echo.out -n 'hello, world!'`
+    assert_equal "hello, world!", `./echo.out -n "hello, world!"`
+    assert_equal "", `./echo.out -n`
+    assert_equal "-n", `./echo.out -n -n`
+  end
+end
