397f34e7a8d1b0f89edd4aec9c43a8d797eab1b4
JIANG Di <jiang-di@outlook.com>
2015-10-11 13:17:40 +0800

    Rewrite test

--- test.rb
+++ test.rb
@@ -1,34 +1,52 @@
 require 'minitest/autorun'
+require "open3"
 
 class TestEcho < Minitest::Test
   CFLAGS = "-std=c99 -Wall"
   `cc #{CFLAGS} echo.c -o echo.out`
 
   def test_basic_usage
-    output = `./echo.out hello, world!`
-    assert_equal "hello, world!\n", output
+    stdout_str, stderr_str, status = Open3.capture3("./echo.out hello, world!")
+    assert_equal "hello, world!\n", stdout_str
+    assert_equal 0, status.exitstatus
+    assert_equal "", stderr_str
   end
 
   def test_non_english
-    output = `./echo.out 你好，世界！`
-    assert_equal "你好，世界！\n", output
+    stdout_str, stderr_str, status = Open3.capture3("./echo.out 你好，世界！")
+    assert_equal "你好，世界！\n", stdout_str
+    assert_equal 0, status.exitstatus
+    assert_equal "", stderr_str
   end
 
   def test_single_quotes
-    output = `./echo.out 'hello, world!'`
-    assert_equal "hello, world!\n", output
+    stdout_str, stderr_str, status = Open3.capture3("./echo.out 'hello, world!'")
+    assert_equal "hello, world!\n", stdout_str
+    assert_equal 0, status.exitstatus
+    assert_equal "", stderr_str
   end
 
   def test_double_quotes
-    output = `./echo.out "hello, world!"`
-    assert_equal "hello, world!\n", output
+    stdout_str, stderr_str, status = Open3.capture3('./echo.out "hello, world!"')
+    assert_equal "hello, world!\n", stdout_str
+    assert_equal 0, status.exitstatus
+    assert_equal "", stderr_str
   end
 
   def test_flag_n
-    assert_equal "hello, world!", `./echo.out -n hello, world!`
-    assert_equal "hello, world!", `./echo.out -n 'hello, world!'`
-    assert_equal "hello, world!", `./echo.out -n "hello, world!"`
-    assert_equal "", `./echo.out -n`
-    assert_equal "-n", `./echo.out -n -n`
+    stdout_str, stderr_str, status = Open3.capture3("./echo.out -n 'hello, world!'")
+    assert_equal "hello, world!", stdout_str
+    assert_equal 0, status.exitstatus
+    assert_equal "", stderr_str
+
+    stdout_str2, stderr_str2, status2 = Open3.capture3("./echo.out -n")
+    assert_equal "", stdout_str2
+    assert_equal 0, status2.exitstatus
+    assert_equal "", stderr_str2
+
+    stdout_str3, stderr_str3, status3 = Open3.capture3("./echo.out -n -n")
+    assert_equal "-n", stdout_str3
+    assert_equal 0, status3.exitstatus
+    assert_equal "", stderr_str3
   end
 end
